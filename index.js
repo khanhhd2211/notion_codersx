57f8695d569e5b25dfa3e520ab891e4862631620
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-06-13 18:22:09 +0700

    bai2

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.set('view engine', 'pug')
+
 app.listen(port, function() {
     console.log(`App are listening on port ${port}`)
 })
 
 app.get('/', function(req, res) {
-    res.send('hello world')
+    res.render('index', { message: '   hello world'})
 })
