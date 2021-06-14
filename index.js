2c5d805e53dd439cdeeff77c6f48159b1d36efe7
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-06-14 09:48:12 +0700

    Bai 4

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+//expressjs configuration
 const express = require('express')
 const app = express()
 const port = 8080
 
+//body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+// get req.body data
+let testPostMethod
+
+//pug
 app.set('view engine', 'pug')
 
 app.listen(port, function() {
@@ -9,5 +19,14 @@ app.listen(port, function() {
 })
 
 app.get('/', function(req, res) {
-    res.render('index', { query: req.query.q })
+    res.render('index', { query: testPostMethod })
+})
+
+app.get('/create', function(req, res) {
+    res.render('create')
+})
+
+app.post('/', function(req, res) {
+    testPostMethod = req.body.testPostMethod
+    res.redirect('/')
 })
