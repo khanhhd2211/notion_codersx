65ea029830f7350aca2a7a41c7a28de8658d2e3d
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-06-15 20:57:30 +0700

    bai 6 - component

---
+++ test-react/src/components/TotoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+    render() {
+        return(
+            <div class="Todo">Toto item</div>
+        );
+    }
+}
+
+export default Todo;
