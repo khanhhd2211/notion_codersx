d8a41fc1ae3fafa55bbad8a1cc963432ab4fdd16
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-06-15 21:35:00 +0700

    bai 7 + bai 8: props, render a list

--- test-react/src/components/TotoItem.js
+++ test-react/src/components/TotoItem.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 
 class Todo extends Component {
+    constructor() {
+        super()
+        this.TodoItems = [ 
+            "Todo item 1", 
+            "Todo item 2", 
+            "Todo item 3"
+        ];
+    }
+
     render() {
         return(
-            <div class="Todo">Toto item</div>
+            <div className="Todo">
+                {
+                    this.TodoItems.map((item, index) => {
+                        return <div key={index}>{item}</div>
+                    })
+                }
+            </div>
         );
     }
 }
